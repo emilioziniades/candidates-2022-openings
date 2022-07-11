3334c8d9f408cd56b794321d64190dc0612912e8
Emilio Ziniades <emilioziniades@protonmail.com>
2022-07-11 22:47:10 +0200

    added gitignore, minor cleaning of main.py

--- main.py
+++ main.py
@@ -8,25 +8,20 @@ from matplotlib.sankey import Sankey
 def main():
     games = load_games()
 
-    # First moves
     first_moves = Counter(game.next().san() for game in games)
 
-    # simple count of openings
     openings = Counter(game.headers["Opening"] for game in games)
 
-    # count of opening categories
     opening_categories = []
     for opening in openings:
         if match := re.search(r"(.*):", opening):
             opening_categories.append(match.group(1))
-        elif ":" not in opening:
+        else:
             opening_categories.append(opening)
     opening_categories = Counter(opening_categories)
-    from IPython import embed; embed(colors='neutral')  # fmt: skip
 
 
 def load_games():
-    pattern = r"(.+), (.+)"
     games = []
     with open("./candidates_2022_all_games.pgn", "r") as all_games_pgn:
         while True:
@@ -36,7 +31,7 @@ def load_games():
             # ensure player names are consistent
             for colour in ["White", "Black"]:
                 player = game.headers[colour]
-                if match := re.search(pattern, player):
+                if match := re.search(r"(.+), (.+)", player):
                     game.headers[colour] = match.group(2) + " " + match.group(1)
 
             games.append(game)
@@ -47,9 +42,20 @@ def load_games():
     assert all(i == 7 for i in white_players.values())
     assert all(i == 7 for i in black_players.values())
     assert len(white_players) == len(black_players) == 8
+    assert len(games) == 56
 
     return games
 
 
 if __name__ == "__main__":
     main()
+
+"""
+TODO
+
+- bar chart of first moves
+- bar chart of opening categories
+- bar char of openings proper
+- Sankey diagram of moves
+- W/D/L bar chart for each opening
+"""
