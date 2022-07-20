c30bd42b4d09efdb675fcdad2fd00119c3638faa
Emilio Ziniades <emilioziniades@protonmail.com>
2022-07-20 15:15:42 +0200

    progress on plots
    - all three basic plots in place
    - data structures are haphazard
    - beginning of migrating all data to pandas df

--- main.py
+++ main.py
@@ -2,26 +2,146 @@ import re
 from collections import Counter
 
 import chess.pgn
-from matplotlib.sankey import Sankey
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+Games = list[chess.pgn.Game]
+
+COLOURS = {
+    "c4": "#114B5F",
+    "d4": "#317B22",
+    "e4": "#F3A712",
+    "Nf3": "#CC001B",
+}
+RESULTS = {
+    "1-0": 1,
+    "1/2-1/2": 0,
+    "0-1": -1,
+}
 
 
-def main():
+def main() -> None:
     games = load_games()
-
-    first_moves = Counter(game.next().san() for game in games)
-
     openings = Counter(game.headers["Opening"] for game in games)
 
+    df = pd.DataFrame()
+    df["opening"] = [game.headers["Opening"] for game in games]
+    df["result"] = [RESULTS[game.headers["Result"]] for game in games]
+    df["opening_category"] = df["opening"].map(parse_opening_category)
+    from IPython import embed; embed(colors='neutral')  # fmt: skip
+
+    # plot_first_moves(games)
+    plot_opening_categories(openings)
+    plot_opening_performance()
+
+
+def plot_first_moves(games: Games) -> None:
+    first_moves = Counter(game.next().san() for game in games)
+    first_move, freq = zip(*sorted(first_moves.items(), key=lambda x: x[0].lower()))
+    colours = [COLOURS[i] for i in first_move]
+    plt.bar(first_move, freq, color=colours)
+    plt.show()
+
+
+def plot_opening_categories(openings: Counter) -> None:
+    first_move_to_opening = {
+        "c4": [
+            "English Opening",
+        ],
+        "d4": [
+            "Queen's Gambit Declined",
+            "Grünfeld Defense",
+            "Catalan Opening",
+            "Nimzo-Indian Defense",
+            "Semi-Slav Defense",
+            "Tarrasch Defense",
+        ],
+        "e4": [
+            "Sicilian Defense",
+            "Ruy Lopez",
+            "Italian Game",
+            "Russian Game",
+            "Four Knights Game",
+        ],
+        "Nf3": [
+            "King's Indian Attack",
+        ],
+    }
+    opening_to_first_move = {
+        opening: move
+        for move, openings in first_move_to_opening.items()
+        for opening in openings
+    }
+
+    opening_categories = count_opening_categories(openings)
+    from IPython import embed; embed(colors='neutral')  # fmt: skip
+
+    opening_categories = sorted(
+        opening_categories.items(),
+        key=lambda x: (opening_to_first_move[x[0]].lower(), 1 / x[1]),
+    )
+    opening_cat, freq = zip(*opening_categories)
+    colours = [COLOURS[opening_to_first_move[i]] for i in opening_cat]
+
+    plt.bar(opening_cat, freq, color=colours)
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.show()
+
+
+def parse_opening_category(opening: str) -> str:
+    if match := re.search(r"(.*):", opening):
+        return match.group(1)
+    else:
+        return opening
+
+
+def count_opening_categories(openings: Counter) -> Counter:
     opening_categories = []
     for opening in openings:
         if match := re.search(r"(.*):", opening):
             opening_categories.append(match.group(1))
         else:
             opening_categories.append(opening)
-    opening_categories = Counter(opening_categories)
+    return Counter(opening_categories)
 
 
-def load_games():
+def count_opening_performance(games: Games, openings: Counter) -> Counter:
+    openings = count_opening_categories(openings).keys()
+
+
+def plot_opening_performance() -> None:
+    category_names = ["White win", "Draw", "Black win"]
+    results = {
+        "QGD": [30, 20, 50],
+        "Ruy Lopez": [50, 30, 20],
+    }
+    labels = list(results.keys())
+    data = np.array(list(results.values()))
+    data_cum = data.cumsum(axis=1)
+    colours = ["#FFFFFF", "#BABABA", "#000000"]
+    bg_colour = "#FCEAC5"
+
+    fig, ax = plt.subplots(facecolor=bg_colour)
+    ax.xaxis.set_visible(False)
+    ax.set_facecolor(bg_colour)
+    ax.set_xlim(0, 100)
+    for i, (colour, colname) in enumerate(zip(colours, category_names)):
+        widths = data[:, i]
+        starts = data_cum[:, i] - widths
+        rects = ax.barh(
+            labels, widths, left=starts, color=colour, label=colname, height=0.3
+        )
+        text_colour = "black" if colour == "#FFFFFF" else "white"
+        ax.bar_label(rects, label_type="center", color=text_colour, fmt="%d%%")
+
+    ax.set_yticks(labels)
+
+    plt.show()
+
+
+def load_games() -> Games:
     games = []
     with open("./candidates_2022_all_games.pgn", "r") as all_games_pgn:
         while True:
@@ -55,7 +175,5 @@ TODO
 
 - bar chart of first moves
 - bar chart of opening categories
-- bar char of openings proper
-- Sankey diagram of moves
 - W/D/L bar chart for each opening
 """
